0036efdcd427318b95bcff8149e13372fa38d5b2
Andrew Peters <anpete@microsoft.com>
2018-03-19 12:44:16 -0700

    WIP

---
+++ PerfBench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerfBench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
