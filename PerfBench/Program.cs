35292932ec7cd10acc56562ed73af09deb9eb0fc
Andrew Peters <anpete@microsoft.com>
2018-03-19 19:21:42 -0700

    WIP

--- PerfBench/Program.cs
+++ PerfBench/Program.cs
@@ -12,24 +12,56 @@ namespace PerfBench
     {
         public static void Main()
         {
-            BenchmarkRunner.Run<BenchmarkFastQuery>();
+            // using (var context = new NorthwindContext())
+            // {
+            //     foreach (var orderDetail in context.OrderDetails)
+            //     {
+            //         Console.WriteLine(orderDetail.OrderID);
+            //     }
+            // }
+
+            using (var b = new BenchmarkFastQuery())
+            {
+                for (var i = 0; i < 5000; i++)
+                {
+                    b.OrderDetails();
+
+                    if (i % 1000 == 0)
+                    {
+                        Console.WriteLine($"Processed {i} queries.");
+                    }
+                }
+            }
+
+            Console.WriteLine("Take snapshot");
+            Console.ReadKey();
+
+            // BenchmarkRunner.Run<BenchmarkFastQuery>();
         }
     }
 
     [MemoryDiagnoser]
     public class BenchmarkFastQuery : IDisposable
     {
-        private readonly FortuneContext _context = new FortuneContext();
+        private readonly FortuneContext _fortuneContext = new FortuneContext();
+        private readonly NorthwindContext _northwindContext = new NorthwindContext();
+
+        //        [Benchmark]
+        //        public void Fortune()
+        //        {
+        //            _fortuneContext.Fortunes.Load();
+        //        }
 
         [Benchmark]
-        public void Load()
+        public void OrderDetails()
         {
-            _context.Fortunes.Load();
+            _northwindContext.OrderDetails.Load();
         }
 
         public void Dispose()
         {
-            _context?.Dispose();
+            _fortuneContext?.Dispose();
+            _northwindContext?.Dispose();
         }
     }
 
@@ -53,5 +85,30 @@ namespace PerfBench
     {
         public int Id { get; set; }
         public string Message { get; set; }
+    }
+
+    public class NorthwindContext : DbContext
+    {
+        public DbQuery<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Database=Northwind;Integrated Security=True;Connect Timeout=60;ConnectRetryCount=0");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Query<OrderDetail>().ToView("Order Details");
+        }
+    }
+
+    public class OrderDetail
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+        public float Discount { get; set; }
     }
 }
